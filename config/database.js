6c77f03228860c0e67c18f5490e4bba2843bfc21
ArnaudM1983 <arnaudmeusy@hotmail.com>
2026-01-28 13:54:58 +0100

    update configuration

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
 const mysql = require("mysql2");
-require("dotenv").config();
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+// Test de connexion
 connection.connect((error) => {
   if (error) {
-    console.error("❌ Erreur de connexion à MySQL:", error.message);
+    console.error("❌ Erreur de connexion à MySQL :", error.message);
     return;
   }
   console.log("✅ Connecté à la base de données MySQL");
 });
+
 module.exports = connection;
