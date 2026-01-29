7b0d226b8a274e934c30f5ea974f105327cd78a2
bertinmick69 <bertinmick@yahoo.fr>
2026-01-29 11:45:53 +0100

    movie-creat

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,3 +1,4 @@
+
 const db = require('../config/database');
 
 const findAll = callback => {
@@ -5,6 +6,51 @@ const findAll = callback => {
   db.query(sql, callback);
 };
 
+const create = (
+  original_title,
+  english_title,
+  submitted_at,
+  youtube_url,
+  cover_image,
+  duration,
+  is_hybrid,
+  original_language,
+  original_synopsis,
+  english_synopsis,
+  creative_process,
+  ia_tools,
+  hasSubs,
+  status,
+  director_id,
+  callback
+) => {
+  const sql =
+    'INSERT INTO movie (original_title,english_title,submitted_at,youtube_url,cover_image,duration,is_hybrid,original_language,original_synopsis,english_synopsis,creative_process,ia_tools,hasSubs,status,director_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+  db.query(
+    sql,
+    [
+      original_title,
+      english_title,
+      submitted_at,
+      youtube_url,
+      cover_image,
+      duration,
+      is_hybrid,
+      original_language,
+      original_synopsis,
+      english_synopsis,
+      creative_process,
+      ia_tools,
+      hasSubs,
+      status,
+      director_id,
+    ],
+    callback
+  );
+};
+
 module.exports = {
   findAll,
+  create,
 };
+
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovie } = require('../controllers/movie.controller');
+const { getAllMovie, createMovie } = require('../controllers/movie.controller');
 
 router.get('/', getAllMovie);
+router.get('/', createMovie);
 module.exports = router;
