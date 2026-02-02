8cfb09fe59ee1f2701b9bd9d0c96d10bf9b61db0
H-rayen <rayen.hasni@laplateforme.io>
2026-02-02 16:26:55 +0100

    movieGetById

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovie, createMovie } = require('../controllers/movie.controller');
+const { getAllMovie, createMovie, getMovieById } = require('../controllers/movie.controller');
 
 router.get('/', getAllMovie);
 router.post('/', createMovie);
+router.get('/:id', getMovieById);
+
 module.exports = router;
