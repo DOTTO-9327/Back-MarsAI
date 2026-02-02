f8ce07c309a7c7b2029a711629c7161458d0c8c2
bertinmick69 <bertinmick@yahoo.fr>
2026-02-02 11:42:54 +0100

    modification createdirector controller

--- models/director.model.js
+++ models/director.model.js
@@ -44,45 +44,7 @@ const create = (
   );
 };
 
-// const update = (
-//   firstname,
-//   lastname,
-//   email,
-//   gender,
-//   birthdate,
-//   country,
-//   city,
-//   phone,
-//   job,
-//   facebook_url,
-//   instagram_url,
-//   youtube_url,
-//   twitter_url,
-//   id,
-//   callback
-// ) => {
-//   const sql = `UPDATE director SET firstname =?,lastname =?,email =?,gender =?,birthdate =?,country =?,city =?,phone =?,job =?,facebook_url =?,instagram_url =?,youtube_url =?,twitter_url =?WHERE id =?`;
-//   db.query(
-//     sql,
-//     [
-//       firstname,
-//       lastname,
-//       email,
-//       gender,
-//       birthdate,
-//       country,
-//       city,
-//       phone,
-//       job,
-//       facebook_url,
-//       instagram_url,
-//       youtube_url,
-//       twitter_url,
-//       id,
-//     ],
-//     callback
-//   );
-// };
+
 const update = (
   firstname,
   lastname,
@@ -100,7 +62,6 @@ const update = (
   id,
   callback
 ) => {
-  // Suppression de la virgule après twitter_url = ?
   const sql = `UPDATE director SET firstname = ?, lastname = ?, email = ?, gender = ?, birthdate = ?, country = ?, city = ?, phone = ?, job = ?, facebook_url = ?, instagram_url = ?, youtube_url = ?, twitter_url = ? WHERE id = ?`;
 
   db.query(
