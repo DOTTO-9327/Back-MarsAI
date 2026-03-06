7eb3c11d97cfe1ec426cf50129252290af648428
bertinmick69 <bertinmick@yahoo.fr>
2026-03-06 13:15:12 +0100

    mail validation

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,4 +1,5 @@
 const emailService = require('../services/email.service');
+const { sendConfirmationEmail } = require('../services/email.service');
 
 const sendSimpleEmail = async (req, res) => {
   try {
@@ -13,8 +14,24 @@ const sendSimpleEmail = async (req, res) => {
     res.status(500).json({ message: "Erreur lors de l'envoi de l'email" });
   }
 };
+const postConfirmation = async (req, res) => {
+  try {
+    const { email, firstname, filmTitle } = req.body;
 
+    if (!email || !firstname) {
+      return res.status(400).json({ message: 'Données manquantes' });
+    }
+
+    await sendConfirmationEmail({ email, firstname, filmTitle });
+
+    res.status(200).json({ message: 'Email envoyé !' });
+  } catch (error) {
+    console.error('Erreur Mailjet Controller:', error);
+    res.status(500).json({ message: "Erreur lors de l'envoi de l'email" });
+  }
+};
 
 module.exports = {
   sendSimpleEmail,
+  postConfirmation,
 };
